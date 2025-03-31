a926cdafb6aec42da859aa22c11b6d05e0d72607
Muskannndas <135194758+Muskannndas@users.noreply.github.com>
2025-03-31 19:23:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # medical_chatbot
+
+# How to run?
+steps:
+1. Clone the repository
+create the conda environment after opening the repository
+conda create -n llmap python-3.10 -y
+conda activate llmap
+2. Install the requirement
+pip install -r requirement.txt
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Genertive AI Project',
+    version='0.0.0',
+    author='Muskan Das',
+    author_email='muskandas02082002@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    
+)
